087381bf62b2b3c66ea1945ee2273bbac163b998
inesa09 <inesusja@gmail.com>
2022-06-23 22:47:36 +1000

    Use Lombok in dtos

--- src/main/java/com/littlepay/trips/dto/Tap.java
+++ src/main/java/com/littlepay/trips/dto/Tap.java
@@ -4,7 +4,11 @@ import java.time.LocalDateTime;
 
 import com.littlepay.trips.enums.Stop;
 import com.littlepay.trips.enums.TapType;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class Tap {
 
 	private Integer id;
@@ -14,71 +18,4 @@ public class Tap {
 	private String companyId;
 	private String busId;
 	private String pan;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public Tap setId(Integer id) {
-		this.id = id;
-		return this;
-	}
-
-	public LocalDateTime getDatetime() {
-		return datetime;
-	}
-
-	public Tap setDatetime(LocalDateTime datetime) {
-		this.datetime = datetime;
-		return this;
-	}
-
-	public TapType getTapType() {
-		return tapType;
-	}
-
-	public Tap setTapType(TapType tapType) {
-		this.tapType = tapType;
-		return this;
-	}
-
-	public Stop getStop() {
-		return stop;
-	}
-
-	public Tap setStop(Stop stop) {
-		this.stop = stop;
-		return this;
-	}
-
-	public String getCompanyId() {
-		return companyId;
-	}
-
-	public Tap setCompanyId(String companyId) {
-		this.companyId = companyId;
-		return this;
-	}
-
-	public String getBusId() {
-		return busId;
-	}
-
-	public Tap setBusId(String busId) {
-		this.busId = busId;
-		return this;
-	}
-
-	public String getPan() {
-		return pan;
-	}
-
-	public Tap setPan(String pan) {
-		this.pan = pan;
-		return this;
-	}
-
-	public static Tap builder() {
-		return new Tap();
-	}
 }
--- src/main/java/com/littlepay/trips/dto/Trip.java
+++ src/main/java/com/littlepay/trips/dto/Trip.java
@@ -4,7 +4,11 @@ import java.time.LocalDateTime;
 
 import com.littlepay.trips.enums.Stop;
 import com.littlepay.trips.enums.TripStatus;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class Trip {
 
 	private LocalDateTime started;
@@ -17,98 +21,4 @@ public class Trip {
 	private String busId;
 	private String pan;
 	private TripStatus status;
-
-	public LocalDateTime getStarted() {
-		return started;
-	}
-
-	public Trip setStarted(LocalDateTime started) {
-		this.started = started;
-		return this;
-	}
-
-	public LocalDateTime getFinished() {
-		return finished;
-	}
-
-	public Trip setFinished(LocalDateTime finished) {
-		this.finished = finished;
-		return this;
-	}
-
-	public Integer getDuration() {
-		return duration;
-	}
-
-	public Trip setDuration(Integer duration) {
-		this.duration = duration;
-		return this;
-	}
-
-	public Stop getFromStop() {
-		return fromStop;
-	}
-
-	public Trip setFromStop(Stop fromStop) {
-		this.fromStop = fromStop;
-		return this;
-	}
-
-	public Stop getToStop() {
-		return toStop;
-	}
-
-	public Trip setToStop(Stop toStop) {
-		this.toStop = toStop;
-		return this;
-	}
-
-	public Double getCharged() {
-		return charged;
-	}
-
-	public Trip setCharged(Double charged) {
-		this.charged = charged;
-		return this;
-	}
-
-	public String getCompanyId() {
-		return companyId;
-	}
-
-	public Trip setCompanyId(String companyId) {
-		this.companyId = companyId;
-		return this;
-	}
-
-	public String getBusId() {
-		return busId;
-	}
-
-	public Trip setBusId(String busId) {
-		this.busId = busId;
-		return this;
-	}
-
-	public String getPan() {
-		return pan;
-	}
-
-	public Trip setPan(String pan) {
-		this.pan = pan;
-		return this;
-	}
-
-	public TripStatus getStatus() {
-		return status;
-	}
-
-	public Trip setStatus(TripStatus status) {
-		this.status = status;
-		return this;
-	}
-
-	public static Trip builder() {
-		return new Trip();
-	}
 }
